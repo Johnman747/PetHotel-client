8ca329787d8b85673ec8ba7eb552b7348d9ff3d5
Johnathan Moes <johnman747@gmail.com>
2019-10-31 16:53:16 -0500

    added table

--- src/App.js
+++ src/App.js
@@ -5,37 +5,64 @@ import axios from "axios";
 class App extends Component {
 
   state = {
-    pets:[]
+    pets: []
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getInfo()
   }
 
-  getInfo = ()=>{
+  getInfo = () => {
     axios.get('/pets')
-    .then((result)=>{
-      result.data.map((pet)=>{
-        this.setState({
-          pets: [...this.state.pets, pet]
+      .then((result) => {
+        result.data.map((pet) => {
+          this.setState({
+            pets: [...this.state.pets, pet]
+          })
         })
+        console.log(this.state.pets)
+      }).catch((err) => {
+        console.log(err);
       })
-      console.log(this.state.pets)
-    }).catch((err)=>{
-      console.log(err);
-    })
-    this.setState({state: this.state})
+    this.setState({ state: this.state })
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.pets.map((pet)=>{
-            return(
-                <p key={pet.id} >{pet.petName}</p>
-            )
-          })}
+          <table>
+            <thead>
+              <tr>
+                <th>Breed</th>
+                <th>Check in date</th>
+                <th>Check in status</th>
+                <th>Color</th>
+                <th>Owner Name</th>
+                <th>Pet Name</th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.pets.map((pet) => {
+              return (
+                <tr key={pet.id} >
+                  <td>{pet.breed}</td>
+                  <td>{pet.checkedInDate}</td>
+                  <td>{pet.checkedInStatus?
+                    <button>True</button>
+                    :
+                    <button>False</button>
+                  }
+                  </td>
+                  <td>{pet.color}</td>
+                  <td>{pet.ownerName}</td>
+                  <td>{pet.petName}</td>
+                </tr>
+              )
+            })}
+            </tbody>
+          </table>
+
         </header>
       </div>
     );
